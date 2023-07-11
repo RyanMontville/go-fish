265007ac472bb9e7e7be4d9fdf7025dc41c5c389
Ryan Montville <ryan.a.montville@gmail.com>
2023-07-11 11:55:10 -0400

    started methods for users turn

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-root',
@@ -6,5 +7,17 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'go-fish';
+  showMenu: boolean = false;
+
+  constructor(private router: Router) {}
+
+  goHome() {
+    this.showMenu = false;
+    this.router.navigate(['/'])
+  }
+
+  goToGame() {
+    this.showMenu = false;
+    this.router.navigate(['/game'])
+  }
 }
--- src/app/game-screen/game-screen.component.ts
+++ src/app/game-screen/game-screen.component.ts
@@ -12,62 +12,70 @@ export class GameScreenComponent implements OnInit {
   userHand: PlayingCard[] = [];
   userPairs: number[] = [];
   userUniqueCards: number[] = [];
-  userCardsClicked: PlayingCard[] = [];
-  userPairNotification: number[] = [];
   programHand: PlayingCard[] = [];
   programPairs: number[] = [];
   programUniqueCards: number[] = [];
-  userHasPair: boolean = false;
-
-  messages: {class: string, text: string}[] = [];
+  gameOver: boolean = false;
+  messages: { class: string, text: string }[] = [];
+  playerTurn: string = '';
 
   ngOnInit() {
-    this.messages.push({class: 'left',text: 'The game has started!'});
+    this.messages.push({ class: 'left', text: 'The game has started!' });
     this.deck.generateDeck();
     for (let i = 0; i < 7; i++) {
       let cardOne: PlayingCard = this.deck.drawCard();
       let cardTwo: PlayingCard = this.deck.drawCard();
-      this.userHand.push(cardOne);
-      if(this.userUniqueCards.includes(cardOne.getRank())){
-        this.userHasPair = true;
-        this.userPairNotification.push(cardOne.getRank());
-      } else {
-        this.userUniqueCards.push(cardOne.getRank());
-      }
+      this.addCardToUserHand(cardOne);
       this.programHand.push(cardTwo);
-      if(this.programUniqueCards.includes(cardTwo.getRank())){
-        this.programPairs.push(cardTwo.getRank());
-        let noPairComputer: PlayingCard[] = this.programHand.filter(card => card.getRank() !== cardTwo.getRank());
-        this.programHand = noPairComputer;
-        let uniqueComputer: number[] = this.programUniqueCards.filter(number => number !== cardTwo.getRank());
-        this.programUniqueCards = uniqueComputer;
-        this.messages.push({class: 'left', text: 'I got a pair of ' + this.formatCardRank(cardTwo.getRank()) + 's'});
-      } else {
-        this.programUniqueCards.push(cardTwo.getRank());
-      }
+      this.checkProgramUniqueCards(cardTwo.getRank());
+    }
+    this.playerTurn = 'user';
+  }
+
+  addCardToUserHand(card: PlayingCard) {
+    this.userHand.push(card);
+    this.checkUserUniqueCards(card.getRank());
+    let checkForFour: PlayingCard[] = this.userHand.filter(currentCard => currentCard.getRank() === card.getRank());
+    if (checkForFour.length === 4) {
+      this.userHand = this.userHand.filter(currCard => currCard.getRank() != card.getRank());
+      this.userPairs.push(card.getRank());
+      this.messages.push({ class: 'right', text: 'I got 4 ' + this.formatCardRank(card.getRank()) + 's' });
+      this.userUniqueCards = this.userUniqueCards.filter(rank => rank != card.getRank());
     }
   }
 
-  checkSuit(suit: string):string {
-    if(suit==='♥' || suit==='♦') {
+  checkUserUniqueCards(rank: number) {
+    if (!this.userUniqueCards.includes(rank)) {
+      this.userUniqueCards.push(rank);
+    }
+  }
+
+  checkProgramUniqueCards(rank: number) {
+    if (!this.programUniqueCards.includes(rank)) {
+      this.programUniqueCards.push(rank);
+    }
+  }
+
+  checkSuit(suit: string): string {
+    if (suit === '♥' || suit === '♦') {
       return "red card";
     }
     return "card";
   }
 
   addMessage(rank: number) {
-    switch(rank) {
-      case 1: this.messages.push({class:'right',text:"Got any As?"}); break;
-      case 11: this.messages.push({class:'right',text:"Got any Js?"}); break;
-      case 12: this.messages.push({class:'right',text:"Got any Qs?"}); break;
-      case 13: this.messages.push({class:'right',text:"Got any Ks?"}); break;
-      default: this.messages.push({class:'right',text:"Got any " + rank + "s?"}); break;
+    switch (rank) {
+      case 1: this.messages.push({ class: 'right', text: "Got any As?" }); break;
+      case 11: this.messages.push({ class: 'right', text: "Got any Js?" }); break;
+      case 12: this.messages.push({ class: 'right', text: "Got any Qs?" }); break;
+      case 13: this.messages.push({ class: 'right', text: "Got any Ks?" }); break;
+      default: this.messages.push({ class: 'right', text: "Got any " + rank + "s?" }); break;
     }
-    
+
   }
 
   formatCardRank(rank: number) {
-    switch(rank) {
+    switch (rank) {
       case 1: return 'A';
       case 11: return 'J';
       case 12: return 'Q';
@@ -76,28 +84,31 @@ export class GameScreenComponent implements OnInit {
     }
   }
 
-  userClicksCard(card: PlayingCard) {
-    this.userCardsClicked.push(card);
-    let findPairs: PlayingCard[] = this.userCardsClicked.filter(currentCard => currentCard.getRank === card.getRank);
-    if(findPairs.length ===2) {
-      let cardOne = findPairs[1];
-      let cardTwo = findPairs[0];
-      let filtered = this.userHand.filter(currCard => currCard.getCard() != cardOne.getCard());
-      this.userHand = filtered;
-      filtered = this.userHand.filter(currCard => currCard.getCard() != cardTwo.getCard());
-      this.userHand = filtered;
-      this.userPairs.push(cardOne.getRank());
-      let filterUnique = this.userUniqueCards.filter(card => card != cardOne.getRank());
-      this.userUniqueCards = filterUnique;
-      let filterNotification = this.userPairNotification.filter(num => num != cardOne.getRank());
-      this.userPairNotification = filterNotification;
-      if(this.userPairNotification.length===0) {
-        this.userHasPair = false;
+  userAsk(card: number) {
+    this.messages.push({ class: 'right', text: 'Got any ' + this.formatCardRank(card) + 's?' });
+    let matches: PlayingCard[] = this.programHand.filter(currentCard => currentCard.getRank() === card);
+    if (matches.length > 0) {
+      this.messages.push({ class: 'left', text: 'Yes, I have ' + matches.length });
+      for (let i = 0; i < matches.length; i++) {
+        this.addCardToUserHand(matches[i]);
+        this.programHand = this.programHand.filter(matchCard => matchCard.getCard() != matches[i].getCard());
       }
-      findPairs = [];
-      this.userCardsClicked = [];
-      this.messages.push({class:'right',text:'I got a pair of ' + this.formatCardRank(cardOne.getRank()) + 's'})
+    } else {
+      this.messages.push({ class: 'left', text: 'No, go fish.' });
+      this.addCardToUserHand(this.deck.drawCard());
+      this.playerTurn = 'program';
+      this.programAsk();
     }
   }
+
+  programAsk() {
+    let min = Math.ceil(0);
+    let max = Math.floor(this.programUniqueCards.length - 1);
+    let randomNumber = Math.floor(Math.random() * (max - min) + min);
+    alert("Program has " + (this.programUniqueCards.length - 1) + ' unique cards. The random number picked was ' + randomNumber);
+    this.playerTurn = 'user';
+  }
+
+  
 
 }
